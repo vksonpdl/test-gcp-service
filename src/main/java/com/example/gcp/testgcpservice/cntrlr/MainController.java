6e07cb537db32fcde64fad20a1b91387e04e14ff
Vikas Patoli <vksonpdl@gmail.com>
2021-12-13 11:37:22 +0530

    commit

---
+++ src/main/java/com/example/gcp/testgcpservice/cntrlr/MainController.java
@@ -0,0 +1,27 @@
+package com.example.gcp.testgcpservice.cntrlr;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+	
+
+
+	
+	@GetMapping("/")
+	public Map<String, String> getHome() {
+		
+		Map<String, String> returnMap = new HashMap<>();
+		returnMap.put("status", "Success");
+	
+		
+		return returnMap;
+	}
+
+}
