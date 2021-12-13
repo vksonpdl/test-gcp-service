15ca60264b1b5519e7af478ce3b14f5d51642e39
Vikas Patoli <vksonpdl@gmail.com>
2021-12-13 11:21:21 +0530

    comit

---
+++ src/main/java/com/example/gcp/testgcpservice/TestGcpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.gcp.testgcpservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGcpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestGcpServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gcp/testgcpservice/TestGcpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gcp.testgcpservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGcpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
